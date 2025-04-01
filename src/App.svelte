ce41844e9b4f81c50c5065ec698b760bb08003c7
lucas <lucasrafaeld03@gmail.com>
2025-04-01 15:40:09 -0300

    Commit inicial

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
   import { calcularArea } from './calculadora.js'; // Importando a função
-  import '.style.css';
 
   let comprimento = 0;
   let largura = 0;
@@ -12,51 +11,6 @@
   }
 </script>
 
-<style>
-  body {
-    margin: 0;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: blue;
-    font-family: Arial;
-  }
-
-  #container {
-    font-size: 25px;
-    border: 6px solid black;
-    padding: 20px;
-    border-radius: 14px;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-  }
-
-  #resultado {
-    margin-top: 20px;
-  }
-
-  #botao {
-    padding: 10px;
-    font-size: 20px;
-    border: 8px;
-    border-radius: 8px;
-    background-color: gray;
-    margin-top: 18px;
-  }
-
-  #botao:hover {
-    background-color: greenyellow;
-  }
-
-  input {
-    font-size: 16px;
-    padding: 10px;
-    margin: 5px 0;
-  }
-</style>
 
 <div id="container">
   <h1>Calculadora de Área Quadrada</h1>
